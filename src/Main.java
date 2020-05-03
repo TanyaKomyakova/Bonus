b24e575aaee44bfdd68ee1a1f3e10ccd0dbd4a8e
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-05-03 16:31:48 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int replenishment = 1500;
+        int bonus;
+
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = 100;
+        int sum = balance + replenishment + bonus;
+
+
+        System.out.println(sum);
+    }
+}
